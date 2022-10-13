181d8130a132340f2e702f0306b38bc068a68fb6
Aaron Spence <aaronspence31@gmail.com>
2022-10-13 10:44:36 -0400

    add users list and page components

---
+++ frontend/src/users/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./UserItem.css";
+
+const UserItem = (props) => {
+  return <h1>User Item</h1>;
+};
+
+export default UserItem;
--- frontend/src/users/pages/Users.js
+++ frontend/src/users/pages/Users.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>This is the USER's page!!!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Charles Malone",
+      image:
+        "https://picjumbo.com/wp-content/uploads/the-golden-gate-bridge-sunset-1080x720.jpg",
+      places: 3,
+    },
+    {
+      id: "u2",
+      name: "Aaron Susserman",
+      image:
+        "https://picjumbo.com/wp-content/uploads/the-golden-gate-bridge-sunset-1080x720.jpg",
+      places: 5,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
